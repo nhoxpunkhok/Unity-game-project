d2d8e2aa5be94ff1e2bce47a50559f3e47b32336
nhoxpunkhok <111383655+nhoxpunkhok@users.noreply.github.com>
2024-03-10 00:25:09 +0700

    update_dashcooldown
    nhung dang bi loi

--- Assets/scripts/player/Dash.cs
+++ Assets/scripts/player/Dash.cs
@@ -6,30 +6,36 @@ using UnityEngine;
 public class Dash : MonoBehaviour
 {
     Movement moveScript;
-
-    public float dashTime;
     public float dashSpeed;
+    public float dashTime;
+    public float dashCooldown;
+    private bool canDash = true;
 
-        private void Start()
+
+    private void Start()
     {
         moveScript = GetComponent<Movement>();
-
     }
     private void Update()
-    {
+    {     
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             StartCoroutine(Dashes());
         }
+
     }
-        IEnumerator Dashes()
+    IEnumerator Dashes()
     {
+        canDash = false;
+       
         float startTime = Time.time;
         while (Time.time < startTime + dashTime)
         {
             moveScript.controller.Move(moveScript.moveDir * dashSpeed * Time.deltaTime);
-
             yield return null;
         }
-    }
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    } 
+ 
 }
